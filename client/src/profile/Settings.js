abfa64f277a297baaf4c1b46646b85418fd5aae0
Shawn Smith <srsmith11@live.com>
2020-09-03 16:00:17 -0600

    stylinlg complete

--- client/src/profile/Settings.js
+++ client/src/profile/Settings.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { AuthConsumer } from '../providers/AuthProvider';
 import Axios from 'axios';
-import FollowForm from './FollowForm';
 import { Button } from 'semantic-ui-react';
 
 function Settings(props) {
@@ -9,24 +8,26 @@ function Settings(props) {
 	const [ email, setEmail ] = useState(props.auth.user.email);
 	const [ username, setUsername ] = useState(props.auth.user.nickname);
 	const [ editing, setEditing ] = useState(false);
-	const [ password, setPassword ] = useState(false);
-	const [ currentPassword, setCurrentPassword ] = useState('');
-	const [ newPassword, setNewPassword ] = useState('');
-	const [ copyPassword, setCopyPassword ] = useState('');
+	const [ disabled, setDisabled ] = useState(true);
+	// const [ password, setPassword ] = useState(false);
+	// const [ currentPassword, setCurrentPassword ] = useState('');
+	// const [ newPassword, setNewPassword ] = useState('');
+	// const [ copyPassword, setCopyPassword ] = useState('');
 
 	const user = {
 		name: firstName,
 		nickname: username,
 		email: email,
-		id: props.auth.user.id,
-		newPassword: newPassword,
-		copyPassword: copyPassword
+		id: props.auth.user.id
+		// newPassword: newPassword,
+		// copyPassword: copyPassword
 	};
 
 	function handleSubmit(e) {
 		e.preventDefault();
 		props.auth.handleEdit(user);
 		setEditing(!editing);
+		setDisabled(!disabled);
 	}
 
 	function removeUser() {
@@ -34,17 +35,26 @@ function Settings(props) {
 		Axios.delete(`/api/users/${user.id}`);
 	}
 
-	function changePassword(e) {
-		e.preventDefault();
-		if (currentPassword === props.auth.user.password && newPassword === copyPassword) {
-			Axios.put(`/api/auth/password`, newPassword);
-			setEditing(!editing);
-		}
-	}
+	// function changePassword(e) {
+	// 	debugger;
+	// 	if (currentPassword === props.auth.user.password && newPassword === copyPassword) {
+	// 		Axios.put(`/api/auth/password`, { password: newPassword });
+	// 		setEditing(!editing);
+	// 	} else if (newPassword !== copyPassword) {
+	// 		alert('Passwords do not match');
+	// 	} else {
+	// 		alert('Invalid password');
+	// 	}
+	// }
 
 	return (
-		<div>
-			<h1 style={{ textAlign: 'center' }}>Account Settings</h1>
+		<div className="settings">
+			<h1 className="settingsHeader" style={{ textAlign: 'center' }}>
+				Account Settings
+			</h1>
+			<div className="settingsButton">
+				<Button icon="edit" color="violet" onClick={() => (setEditing(!editing), setDisabled(!disabled))} />
+			</div>
 			<form onSubmit={handleSubmit}>
 				<p>
 					Name:
@@ -52,6 +62,7 @@ function Settings(props) {
 						label="First Name"
 						value={firstName}
 						onChange={editing ? (e) => setFirstName(e.target.value) : null}
+						disabled={disabled}
 					/>
 				</p>
 				<p>
@@ -60,49 +71,64 @@ function Settings(props) {
 						label="Username"
 						value={username}
 						onChange={editing ? (e) => setUsername(e.target.value) : null}
+						disabled={disabled}
 					/>
 				</p>
 				<p>
 					Email:
-					<input label="Email" value={email} onChange={editing ? (e) => setEmail(e.target.value) : null} />
+					<input
+						label="Email"
+						value={email}
+						onChange={editing ? (e) => setEmail(e.target.value) : null}
+						disabled={disabled}
+					/>
 				</p>
-				{editing ? <button>Submit</button> : null}
+				{editing ? (
+					<Button color="green" size="small">
+						Submit
+					</Button>
+				) : null}
 			</form>
 			<br />
-			<button onClick={() => setEditing(!editing)}>Edit</button>
-			{editing ? <button onClick={() => setPassword(!password)}>Change Password</button> : null}
+			{/* {editing ? <button onClick={() => setPassword(!password)}>Change Password</button> : null}
 			<form onSubmit={changePassword}>
 				{password && editing ? (
 					<div>
-						<p>
-							Current Password:
-							<input
-								label="Current Password"
-								type="password"
-								value={currentPassword}
-								onChange={editing ? (e) => setCurrentPassword(e.target.value) : null}
-							/>
-						</p>
-						<p>
-							New Password:
-							<input
-								label="New Password"
-								value={newPassword}
-								onChange={editing ? (e) => setNewPassword(e.target.value) : null}
-							/>
-						</p>
-						<p>
-							Retype Password:
-							<input
-								label="Retype Password"
-								value={copyPassword}
-								onChange={editing ? (e) => setCopyPassword(e.target.value) : null}
-							/>
-						</p>
-						<button>Submit</button>
+						<Form onSubmit={changePassword}>
+							<p>
+								Current Password:
+								<input
+									label="Current Password"
+									type="password"
+									value={currentPassword}
+									onChange={editing ? (e) => setCurrentPassword(e.target.value) : null}
+								/>
+							</p>
+							<p>
+								New Password:
+								<input
+									label="New Password"
+									type="password"
+									value={newPassword}
+									onChange={editing ? (e) => setNewPassword(e.target.value) : null}
+								/>
+							</p>
+							<p>
+								Retype Password:
+								<input
+									label="Retype Password"
+									type="password"
+									value={copyPassword}
+									onChange={editing ? (e) => setCopyPassword(e.target.value) : null}
+								/>
+							</p>
+							<Button color="green" size="tiny">
+								Submit
+							</Button>
+						</Form>
 					</div>
 				) : null}
-			</form>
+			</form> */}
 			<Button color="red" onClick={() => removeUser()}>
 				Delete Account
 			</Button>
